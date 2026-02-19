6193f2f91ceb09d33bde44be21829cc6c85aacca
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2026-02-19 20:21:07 +0300

    Handle processing arbitrary file and writing table into arbitrary file

--- CSVPrettyPrinter/CSVPrettyPrinterTest.c
+++ CSVPrettyPrinter/CSVPrettyPrinterTest.c
@@ -4,18 +4,35 @@
 #include <string.h>
 int main(int argc, char** argv)
 {
-    FILE* fp = fopen("biostats.csv", "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Failed to open \"%s\": %s\n", "csv_test.csv", strerror(errno));
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s [input *.csv filename] [output filename]\n", argv[0]);
         return 1;
     }
-    CSVData* data = CSVDataRead(fp);
+    FILE* fpin = fopen( argv[1], "r");
+    if (fpin == NULL) {
+        fprintf(stderr, "Failed to open \"%s\": %s\n", argv[1], strerror(errno));
+        return 1;
+    }
+    CSVData* data = CSVDataRead(fpin);
+    fclose(fpin);
     if (data == NULL) {
-        fprintf(stderr, "Failed to parse \"%s\"\n", "biostats.csv");
+        fprintf(stderr, "Failed to parse \"%s\".\n", argv[1]);
         return 1;
     }
-    if (!CSVDataWrite(data, stdout))
-        fprintf(stderr, "Failed to write!\n");
+
+    FILE* fpout = fopen(argv[2], "w");
+    if (fpout == NULL) {
+        CSVDataFree(&data);
+        fprintf(stderr, "Failed to open for writing \"%s\": %s\n", argv[2], strerror(errno));
+        return 1;
+    }
+    if (!CSVDataWrite(data, fpout)) {
+        fclose(fpout);
+        CSVDataFree(&data);
+        fprintf(stderr, "Failed to write to \"%s\".\n", argv[2]);
+        return 1;
+    }
+    fclose(fpout);
     CSVDataFree(&data);
     return 0;
 }
