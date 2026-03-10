66119946f2e198dee9e47736abc5fcf572005d90
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2026-03-10 19:42:52 +0300

    Implemented reading commands

--- sources/Aerosoft/aerosoft.c
+++ sources/Aerosoft/aerosoft.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <ctype.h>
 
 #define STRINGINIZE(val) STRINGINIZE_DUMMY(val)
 #define STRINGINIZE_DUMMY(val) #val
@@ -30,7 +31,6 @@ AVLTree* loadData(FILE* file)
         return NULL;
     }
     
-    char buf[BUFSIZ] = {};
     char key[KEY_SIZE+1] = {};
     char value[VALUE_SIZE+1] = {};
     while (!feof(file)) {
@@ -72,6 +72,60 @@ AVLTree* loadData(FILE* file)
     return tree;
 }
 
+void findRecord(AVLTree* tree, char* key)
+{
+
+}
+
+void addRecord(AVLTree* tree, char* keyValue)
+{
+
+}
+
+void deleteRecord(AVLTree* tree, char* key)
+{
+
+}
+
+void saveRecords(FILE* file, AVLTree* tree)
+{
+
+}
+
+void printHelp()
+{
+    printf("Commands:\n");
+    printf("'find [key]' is used to print a record by key.\n");
+    printf("'add [key:value]' is used to add a new record temporarily.\n");
+    printf("'delete [key]' is used to delete a record by key temporarily.\n");
+    printf("'save' is used to write temporarily data in the opened file.\n");
+    printf("'quit' is used to quit\n");
+}
+
+/*
+ * Return false if user wants to quit.
+ * Return true otherwise.
+ */
+bool processCommand(AVLTree* tree, FILE* file, char* command, char* specifier)
+{
+    if (strcmp("find", command) == 0) {
+        findRecord(tree, specifier);
+    } else if (strcmp("add", command) == 0) {
+        addRecord(tree, specifier);
+    } else if (strcmp("delete", specifier) == 0) {
+        deleteRecord(tree, specifier);
+    } else if (strcmp("save", command) == 0) {
+        saveRecords(file, tree);
+    } else if (strcmp("quit", command) == 0) {
+        return false;
+    } else if(strcmp("help", command) == 0) {
+        printHelp();
+    } else {
+        fprintf(stderr, "Undefined command. Try 'help'.\n");
+    }
+    return true;
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 2) {
@@ -90,8 +144,29 @@ int main(int argc, char** argv)
         fprintf(stderr, "Failed to load data from \"%s\".\n", argv[1]);
         return 1;
     }
-
     printf("%d records are loaded, ready to work.\n", avlSize(tree));
+
+    char buf[1024] = {};
+    while (true) {
+        printf("> ");
+        if (fgets(buf, sizeof(buf), stdin) == NULL)
+            break;
+        
+        buf[strcspn(buf, "\n")] = '\0';
+        int idx = 0;
+        while (isspace(buf[idx]))
+            idx++;
+
+        
+        char command[8] = {};
+        char specifier[256] = {}; 
+        if (sscanf(buf + idx, "%7[^ \n] %[^ \n]\n", command, specifier) < 0)
+            break;
+        printf ("Command = %s, specifier = %s\n", command, specifier);
+        if (!processCommand(tree, file, command, specifier))
+            break;
+    }
+
     fclose(file);
     avlFree(&tree);
     return 0;
