3c4fded46d0469e9c8a3e80f3d79b47daf97eb5c
Maxim Gavrilov <maksim.gavrilov.0@mail.ru>
2026-03-21 08:50:15 +0300

    Started implementing heap

---
+++ sources/Heap/heap.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef struct Heap Heap;
+
+/*
+ * Comparator for structuring heap values
+ */
+typedef int (*HeapComparator) (int, int);
+
+/*
+ * Create heap with given numbers and comparator.
+ * Create empty heap if nums == NULL and count == 0,
+ * otherwise return NULL or if errors occured
+ */
+Heap* heapCreate(HeapComparator comp, unsigned count, int* nums);
+
+/*
+ * Push number into the heap.
+ * Return false if error occured.
+ */
+bool heapPush(Heap* heap, int num);
+
+/*
+ * Remove and return value from the top of the heap.
+ * If heap is NULL or empty return 0 always.
+ * User must check the size of the heap manually via heapSize() or heapEmpty().
+ */
+int heapPop(Heap* heap);
+
+/*
+ * Return true if heap is empty or NULL pointer.
+ */
+bool heapEmpty(const Heap* heap);
+
+/*
+ * Return the size of the heap or 0 is NULL pointer.
+ */
+unsigned heapSize(const Heap* heap); 
+
+/*
+ * Free data allocated for the heap and sets heap pointer to NULL
+ */
+void heapFree(Heap** heap);
